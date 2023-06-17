2888b68de9c2a10b924916a0fd7436dcb5e321a9
SwthSawTheme <sawtheme1@gmail.com>
2023-06-17 19:46:07 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 2
+
+pyautogui.press("win")
+pyautogui.write("LeagueClient")
+pyautogui.press("Enter")
+
+time.sleep(3)
+
+pyautogui.write('test')
+pyautogui.press('TAB')
+pyautogui.write('test')
+pyautogui.press('Enter')
+
+time.sleep(2)
+
+pyautogui.press('Enter')
+
+time.sleep(30)
+
+pyautogui.click(x = 760, y = 105)
+
+time.sleep(3)
+
+pyautogui.press('win')
+pyautogui.write('Bloco de Notas')
+pyautogui.press('Enter')
+pyautogui.write('Low elo')
+
+time.sleep(3)
+
+pyautogui.hotkey('alt', "TAB")
+
+time.sleep(3)
+
+pyautogui.click(x = 846, y = 162)
+pyautogui.write('ImNuubz')
+pyautogui.press('Enter')
+
+time.sleep(5)
+
+pyautogui.click(x = 569, y = 156)
+
+time.sleep(5)
+
+pyautogui.hotkey('alt', 'TAB')
+pyautogui.press('Enter')
+time.sleep(2)
+pyautogui.write('Só perde, ruim pra krl...Slc')
+
+time.sleep(3)
+
+pyautogui.hotkey('alt','TAB')
+
+time.sleep(2)
+
+pyautogui.click(x = 448, y = 152)
+
+time.sleep(5)
+
+pyautogui.hotkey('alt', 'TAB')
+pyautogui.hotkey('Enter')
+
+pyautogui.write('Perfil feio, sem trofeu...credo!')
+
+time.sleep(3)
+
+pyautogui.hotkey('alt','TAB')
+
+time.sleep(3)
+
+pyautogui.click(x = 277, y = 108)
+
+time.sleep(3)
+
+pyautogui.click(x = 606, y = 622)
+pyautogui.click(x = 606, y = 622)
+
